cb665c9c0b359f8d998dbf2032ffa9b602a15f1d
Casey Price <caseyprice217@gmail.com>
2019-01-31 11:18:09 -0800

    print names/prices of cakes/cookies to .txt file

--- Honey4GroceriesSelenium/SeleniumTutorial/costcoBread.py
+++ Honey4GroceriesSelenium/SeleniumTutorial/costcoBread.py
@@ -5,3 +5,14 @@ browser = webdriver.Chrome()
 url = "https://www.costco.com/cakes-cookies.html"
 browser.get(url)
 
+items = browser.find_elements_by_class_name("product")
+
+text_file = open("output.txt", "w")
+
+for x in range (0, len(items)):
+    element_name = items[x].find_element_by_class_name("description")
+    element_price = items[x].find_element_by_class_name("price")
+
+    text_file.write("%s\n" % element_name.text)
+    text_file.write("%s\n" % element_price.text)
+    text_file.write("\n")
