b7c3a2f54c99f754d2cf46da14817061fc8233d7
vivianweiii <law045@ucsd.edu>
2019-02-14 09:44:45 -0800

    reorganized dir

--- History/scrapyCostco/scrapeCostco/spiders/homepage_spider.py
+++ History/scrapyCostco/scrapeCostco/spiders/homepage_spider.py
@@ -5,7 +5,9 @@ class HomepageSpider(scrapy.Spider):
     name = "homepage"
 
     def start_requests(self):
-        headers = {'User-Agent':'Wget/1.11.4', 'Accept':'*/*', \
+        headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel \
+                Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) \
+                Chrome/71.0.3578.98 Safari/537.36', 'Accept':'*/*', \
                 'Connection':'Keep-Alive'}
         yield scrapy.Request(url='https://www.costco.com/grocery-household.html',\
                 callback=self.parse,\
