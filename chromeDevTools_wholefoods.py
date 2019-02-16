d46fc4c54d33eebcd884d5c1cb5a94af2f8042d6
lishane31 <sil218@ucsd.edu>
2019-02-15 19:06:53 -0800

    scroll to end (incomplete)

--- chromeDevTools_wholefoods.py
+++ chromeDevTools_wholefoods.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 import pychrome
 import json
+import time
 
 requestIDs= []
 
 def request_will_be_sent(**kwargs):
+    print("1")
     if 'api/search?' in kwargs.get('request').get('url'):
         requestIDs.append(kwargs.get('requestId'))
         print(kwargs.get('request').get('url'))
@@ -13,7 +15,7 @@ url = 'https://products.wholefoodsmarket.com/search?sort=relevance&store=10066&c
 
 options = webdriver.ChromeOptions()
 
-options.add_argument('headless')
+options.add_argument('--no-sandbox')
 options.add_argument('remote-debugging-port=9222')
 
 driver = webdriver.Chrome(chrome_options=options)
@@ -26,6 +28,27 @@ tab.set_listener('Network.requestWillBeSent', request_will_be_sent)
 tab.start()
 tab.call_method('Network.enable')
 tab.call_method('Page.navigate', url=url, _timeout=10)
+
+
+SCROLL_PAUSE_TIME = 0.5
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    print("2")
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
 
 tab.wait(10)
 
