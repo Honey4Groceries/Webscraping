b809610d3a8a8dd464ce478dd763798e0a9d203e
lishane31 <sil218@ucsd.edu>
2019-02-20 17:06:05 -0800

    scrolling done yayers -Shane

--- chromeDevTools_wholefoods.py
+++ chromeDevTools_wholefoods.py
@@ -1,7 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 import pychrome
 import json
 import time
+
+'''
+tried using 2 tabs to see if one of them will scroll
+
+tried scrolling by smaller increments
+
+tried sleeping to load the page before scrolling
+
+tried closing all tabs
+
+tried both ActionChains and driver.scrollTo, both only scrolls old tabs and not
+the newest one >:(((((((((((((((((
+
+tried changing from driver scroll to tab scroll (tab has no scroll)
+
+'''
 
 requestIDs= []
 
@@ -24,28 +42,37 @@ driver = webdriver.Chrome(chrome_options=options)
 browser = pychrome.Browser(url='http://127.0.0.1:9222')
 tab = browser.new_tab()
 
-tab.set_listener('Network.requestWillBeSent', request_will_be_sent)
+# SWITCH THE FOCUS TAB THIS IS THE LINE THAT FINALLY FIXED IT OMG
+driver.switch_to.window(driver.window_handles[1])
 
+
+tab.set_listener('Network.requestWillBeSent', request_will_be_sent)
 tab.start()
 tab.call_method('Network.enable')
 tab.call_method('Page.navigate', url=url, _timeout=10)
 
-
 SCROLL_PAUSE_TIME = 0.5
 
 # Get scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
-
+last_height = driver.execute_script("return document.documentElement.scrollHeight")
+actions = ActionChains(driver)
 while True:
-    print("2")
-    # Scroll down to bottom
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    # Wait to load page
-    time.sleep(SCROLL_PAUSE_TIME)
+    #using action chains to scroll
+    actions.send_keys(Keys.PAGE_DOWN).perform()
+    actions.send_keys(Keys.PAGE_DOWN).perform()
+    time.sleep(1)
+
+    actions.send_keys(Keys.PAGE_DOWN).perform()
+    actions.send_keys(Keys.PAGE_DOWN).perform()
+    time.sleep(1)
 
     # Calculate new scroll height and compare with last scroll height
-    new_height = driver.execute_script("return document.body.scrollHeight")
+    new_height = driver.execute_script("return document.documentElement.scrollHeight")
+ 
+    print(new_height)
+    print(last_height)
+
     if new_height == last_height:
         break
     last_height = new_height
@@ -54,6 +81,6 @@ while True:
 tab.wait(10)
 
 for requestID in requestIDs:
-    print(json.dumps(json.loads(tab.call_method('Network.getResponseBody', requestId=requestID)['body']), indent=4))
+    print(json.dumps(json.loads(tab.call_method('Network.getResponseBody', requestId=requestID)['body']), indent=4) + '\n\n\n\n\n\n^^^^^^^^^^^^^^^^^^^^')
 tab.stop()
-
+driver.quit()
